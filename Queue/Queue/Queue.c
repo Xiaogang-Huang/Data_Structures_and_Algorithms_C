6a8a885b5e2483ab9d04b192755657fb3f3d4d79
晓港 黄 <5193792@qq.com>
2021-03-02 11:57:34 +0800

    用数组实现的队列

--- Queue/Queue/Queue.c
+++ Queue/Queue/Queue.c
@@ -1,69 +1,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct List {
-	int item;
-	struct List* next;
+#define Error(str) FatalError(str)
+#define FatalError(str) fprintf(stderr, "%s\n", str), exit(1)
+
+typedef int ElementType;
+
+struct QueueRecord {
+	int Capacity; //队列的最大容量
+	int Front;  //队首的位置
+	int Rear;  //队尾的位置
+	int Size; 
+	ElementType* Array;
 };
 
-struct Queue {
-	int size;
-	struct List* head;
-	struct List* rear;
-};
-typedef struct Queue* qlink;
+typedef struct QueueRecord* Queue;
 
-qlink create_queue(void) {
-	qlink queue = malloc(sizeof(struct Queue));
-	if (!queue)
-		return queue;
-	queue->size = 0;
-	queue->head = queue->rear = NULL;
-	return queue;
+int IsEmpty(Queue Q)
+{
+	return Q->Size == 0;
 }
 
-int empty(qlink queue) {
-	return queue->size == 0;
+int IsFull(Queue Q)
+{
+	return Q->Size == Q->Capacity;
 }
 
-void enqueue(qlink queue, int item) {
-	struct List* new = malloc(sizeof(struct List));
-	if (!new)
-		return;
-	new->item = item;
-	new->next = NULL;
-	if (queue->head)
-		queue->rear->next = new;
+void MakeEmpty(Queue Q)
+{
+	Q->Size = 0;
+	Q->Front = 1;
+	Q->Rear = 0;
+}
+
+Queue CreateQueue(int MaxElements)
+{
+	Queue Q;
+	
+	Q = malloc(sizeof(struct QueueRecord));
+	if (Q == NULL)
+		FatalError("Out of space!!!");
+
+	Q->Array = malloc(MaxElements * sizeof(ElementType));
+	if (Q->Array == NULL)
+		FatalError("Out of space!!!");
+
+	Q->Capacity = MaxElements;
+	MakeEmpty(Q);
+
+	return Q;
+}
+
+void DisposeQueue(Queue Q)
+{
+	if (Q != NULL)
+	{
+		free(Q->Array);
+		free(Q);
+	}
+}
+
+static int Succ(int Value, Queue Q)
+{
+	if (++Value == Q->Capacity)
+	{
+		Value = 0;
+	}
+	return Value;
+}
+
+void Enqueue(ElementType X, Queue Q)
+{
+	if (IsFull(Q))
+		Error("Full queue");
 	else
-		queue->head = new;
-	queue->rear = new;
-	queue->size++;
+	{
+		Q->Size++;
+		Q->Rear = Succ(Q->Rear, Q);
+		Q->Array[Q->Rear] = X;
+	}
 }
 
-int dequeue(qlink queue) {
-	if (empty(queue))
-		return -1;
-	int retval = queue->head->item;
-	struct List* torm = queue->head;
-	if (queue->head == queue->rear)
-		queue->rear = NULL;
-	queue->head = queue->head->next;
-	free(torm);
-	queue->size--;
-	return retval;
+ElementType Front(Queue Q)
+{
+	return Q->Array[Q->Front];
+}
+
+void Dequeue(Queue Q)
+{
+	if (IsEmpty(Q))
+		Error("Empty queue");
+	else
+	{
+		Q->Size--;
+		Q->Front = Succ(Q->Front, Q);
+	}
+}
+
+void PrintQueue(Queue Q)
+{
+	int i;
+
+	if (IsEmpty(Q))
+		Error("Empty queue");
+	else
+	{
+		printf("Q->Size = %d\n", Q->Size);
+		printf("Q->Front = %d\n", Q->Front);
+		printf("Q->Rear = %d\n", Q->Rear);
+		for (i = Q->Front; i != Q->Rear; i = Succ(i, Q))
+			printf("%d\n", Q->Array[i]);
+	}
+		
 }
 
 int main() {
-	int data[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int i = 0;
-	qlink queue = create_queue();
-	while (i < 5)
-		enqueue(queue, data[i++]);
-	while (!empty(queue))
-		printf("%d\n", dequeue(queue));
-	while (i < 10)
-		enqueue(queue, data[i++]);
-	while (!empty(queue))
-		printf("%d\n", dequeue(queue));
+	int A[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0, N = sizeof(A) / sizeof(A[0]);
+	Queue Q;
+	
+	Q = CreateQueue(N);
+
+	for (i = 0; i < 8; i++)
+		Enqueue(A[i], Q);
+	PrintQueue(Q);
+	Dequeue(Q);
+	PrintQueue(Q);
+	for (i = 8; i < N; i++)
+		Enqueue(A[i], Q);
+	PrintQueue(Q);
+	Enqueue(10, Q);
+	PrintQueue(Q);
+	DisposeQueue(Q);
 	return 0;
 }
